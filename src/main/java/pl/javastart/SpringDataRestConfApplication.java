cdb4aeb694dd37e446bb828954b8c249fb7252a9
tstepnik <tomasz.stepnik77@gmail.com>
2020-03-20 11:24:09 +0100

    add essential files

---
+++ src/main/java/pl/javastart/SpringDataRestConfApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestConfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRestConfApplication.class, args);
+    }
+
+}

8c5ce713e63c7be3d48361cfa509a66c92004c56
tstepnik <tomasz.stepnik77@gmail.com>
2020-03-20 12:54:41 +0100

    finally app

--- src/main/java/pl/javastart/model/Product.java
+++ src/main/java/pl/javastart/model/Product.java
@@ -1,4 +1,71 @@
 package pl.javastart.model;
 
-public class Product {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 8562653542799920827L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Producer producer;
+
+    private double price;
+    private int quantity;
+
+    public Product(){}
+
+    Product(String name, Producer producer, double price, int quantity){
+        this.name=name;
+        this.producer=producer;
+        this.price=price;
+        this.quantity=quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Producer getProducer() {
+        return producer;
+    }
+
+    public void setProducer(Producer producer) {
+        this.producer = producer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
